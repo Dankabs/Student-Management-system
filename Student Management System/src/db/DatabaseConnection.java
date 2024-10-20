752fa67623f15f4f9f0e49efc45f8ef5697f8d81
Dankabs <166226697+Dankabs@users.noreply.github.com>
2024-10-20 02:19:17 +0200

    Update DatabaseConnection.java

--- Student Management System/src/db/DatabaseConnection.java
+++ Student Management System/src/db/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_management";
-    private static final String USERNAME = "root"; 
-    private static final String PASSWORD = "merveille";   
+    private static final String USERNAME = "your_username"; 
+    private static final String PASSWORD = "your_password";   
     public static Connection getConnection() {
         Connection connection = null;
         try {
